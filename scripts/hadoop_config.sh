1b6e70874de59da72384563d7be6e0166edc75df
Noam Konja <Noamk@elad.co.il>
2024-01-30 19:58:28 +0200

    NK: no change

--- scripts/hadoop_config.sh
+++ scripts/hadoop_config.sh
@@ -3,3 +3,7 @@ echo 'Exist directory: '
 pwd
 python3 -m pip install requests
 python3 -m pip install urllib3==1.26.6
+
+export PATH="/usr/local/bin:$PATH"
+source ~/.bash_profile
+echo 'Print PATH: ' $PATH
